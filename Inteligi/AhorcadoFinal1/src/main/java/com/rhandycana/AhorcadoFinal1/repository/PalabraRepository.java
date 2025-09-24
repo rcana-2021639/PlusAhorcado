08dcf87d46d3e2c1803c28b2d4b2bee7834e3816
rcana-2021639 <rcana-2021639@kinal.edu.gt>
2025-09-23 23:28:39 -0600

    Feat: se implementó la interfaz PalabraRepository y UsuarioRepository extendiendo JpaRepository para la gestión de palabras y usuarios

--- Inteligi/AhorcadoFinal1/src/main/java/com/rhandycana/AhorcadoFinal1/repository/PalabraRepository.java
+++ Inteligi/AhorcadoFinal1/src/main/java/com/rhandycana/AhorcadoFinal1/repository/PalabraRepository.java
@@ -1,4 +1,8 @@
 package com.rhandycana.AhorcadoFinal1.repository;
 
-public interface PalabraRepository {
+import com.rhandycana.AhorcadoFinal1.model.Palabra;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PalabraRepository extends JpaRepository<Palabra, Integer> {
+    boolean existsByPalabraAndCategoria(String palabra, String categoria);
 }
--- Inteligi/AhorcadoFinal1/src/main/java/com/rhandycana/AhorcadoFinal1/repository/UsuarioRepository.java
+++ Inteligi/AhorcadoFinal1/src/main/java/com/rhandycana/AhorcadoFinal1/repository/UsuarioRepository.java
@@ -1,4 +1,9 @@
 package com.rhandycana.AhorcadoFinal1.repository;
 
-public interface UsuarioRepository {
+import com.rhandycana.AhorcadoFinal1.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    boolean existsByCorreoUsuario(String correoUsuario);
+    boolean existsByNombreUsuarioAndApellidoUsuario(String nombreUsuario, String apellidoUsuario);
 }
