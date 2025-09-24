2dabc571fd4298f7efd1bb8bbd34468f888afe5d
rcana-2021639 <rcana-2021639@kinal.edu.gt>
2025-09-23 23:26:21 -0600

    Feat: se agregó la clase Palabra y Usuario con sus atributos y anotaciones JPA

--- Inteligi/AhorcadoFinal1/src/main/java/com/rhandycana/AhorcadoFinal1/model/Palabra.java
+++ Inteligi/AhorcadoFinal1/src/main/java/com/rhandycana/AhorcadoFinal1/model/Palabra.java
@@ -1,4 +1,23 @@
 package com.rhandycana.AhorcadoFinal1.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "palabras")
 public class Palabra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_palabra")
+    private Integer codigoPalabra;
+
+    @Column(name = "palabra")
+    private String palabra;
+
+    @Column(name = "pista")
+    private String pista;
+
+    @Column(name = "categoria")
+    private String categoria;
 }
--- Inteligi/AhorcadoFinal1/src/main/java/com/rhandycana/AhorcadoFinal1/model/Usuario.java
+++ Inteligi/AhorcadoFinal1/src/main/java/com/rhandycana/AhorcadoFinal1/model/Usuario.java
@@ -1,4 +1,31 @@
 package com.rhandycana.AhorcadoFinal1.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_usuario")
+    private Integer codigoUsuario;
+
+    @Column(name = "nombre_usuario")
+    private String nombreUsuario;
+
+    @Column(name = "apellido_usuario")
+    private String apellidoUsuario;
+
+    @Column(name = "correo_usuario")
+    private String correoUsuario;
+
+    @Column(name = "contraseña_usuario")
+    private String contraseñaUsuario;
+
+    @Column(name = "fecha_registro")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaRegistro;
 }
