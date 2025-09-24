deff36c6351a3c02823d79683c7775057e32596d
rcana-2021639 <rcana-2021639@kinal.edu.gt>
2025-09-23 23:14:19 -0600

    Feat: se agregó la configuración inicial del proyecto con Maven, incluyendo archivos de propiedades y clases principales para la aplicación Spring Boot

---
+++ Inteligi/AhorcadoFinal1/src/main/java/com/rhandycana/AhorcadoFinal1/AhorcadoFinal1Application.java
@@ -0,0 +1,13 @@
+package com.rhandycana.AhorcadoFinal1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhorcadoFinal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhorcadoFinal1Application.class, args);
+	}
+
+}
---
+++ Inteligi/AhorcadoFinal1/src/test/java/com/rhandycana/AhorcadoFinal1/AhorcadoFinal1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhandycana.AhorcadoFinal1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhorcadoFinal1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
